8d3f184745f8418fb901fc6d83fa1b9c8ebd7a60
MacBook <macbook@MacBook-Air-MacBook.local>
2024-07-20 23:17:21 +0500

    My first commit

---
+++ configuration.py
@@ -0,0 +1,12 @@
+
+URL_SERVICE = "https://24348677-3718-4f20-8271-b9ebe3bd9ef3.serverhub.praktikum-services.ru"
+
+DOC_PATH = "/docs/"
+
+LOG_MAIN_PATH = "/api/logs/main/"
+
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+
+CREATE_USER_PATH = "/api/v1/users/"
+
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
